227661b07feadf05eb922ced474012c5bc66425b
maas wu <maasdruck@gmail.com>
2017-02-28 19:33:20 +0800

    抓取解析爱卡汽车口碑 1.01
    单页点评角度和点评写入数据库
    创建数据库

--- newcar.php
+++ newcar.php
@@ -8,7 +8,86 @@
   * @compatibility 支持 php 5.3 以上版本
   */
 
-// 测试解析单一列表页
+$sql_create_p = @$_GET['sql_create_p'];
+$sql_delete_p = @$_GET['sql_delete_p'];
+$sql_write_p = @$_GET['sql_write_p'];
+
+echo '
+<div>
+    <form method="get" action="newcar.php">';
+    echo '
+        <span>
+            <input type="checkbox" name="sql_create_p';
+    if (@$_GET['sql_create_p'] == 'on') {
+        echo  '" checked="checked" value="on';
+    }
+    echo '">创建表格
+            <input type="checkbox" name="sql_delete_p';
+    if (@$_GET['sql_delete_p'] == 'on') {
+        echo  '" checked="checked" value="on';
+    }
+    echo '">删除表格
+            <input type="checkbox" name="sql_write_p';
+    if (@$_GET['sql_write_p'] == 'on') {
+        echo  '" checked="checked" value="on';
+    }
+    echo '">写入数据
+        </span>
+        <input type="submit" value="执行">
+    </form>
+</div>
+';
+
+// 初始化数据库
+$con = mysqli_init();
+
+if(!$con) {
+    echo 'mysqli_init error';
+    exit(0);
+}
+
+// 登录信息
+$switch = 0;
+if ($switch == 0) {
+    $ret = mysqli_real_connect($con, 'localhost', 'root', 'asdf', 'crawl', 3306);
+}
+
+if(!$ret) {
+    echo 'mysqli_real_connect error';
+    exit(0);
+}
+
+// 中文编码，保证传进 mysql 中文不出现乱码
+$con -> set_charset('utf8');
+
+// 设置时区
+date_default_timezone_set('Asia/Shanghai');
+
+// 创建表格
+if ($sql_create_p == 'on') {
+    $sql = "create table xcar_newcar (
+        id int not null primary key auto_increment,
+        na_1s char(2) not null,
+        na_2s char(2) not null,
+        update_time datetime not null
+    )";
+    $ret = mysqli_query($con, $sql);
+    if(!$ret) {
+        echo 'mysqli_query error';
+        exit(0);
+    }
+}
+
+// 删除表格
+if ($sql_delete_p == 'on') {
+    $sql = "drop table xcar_newcar";
+    $ret = mysqli_query($con, $sql);
+    if(!$ret) {
+        echo 'mysqli_query error';
+        exit(0);
+    }
+}
+
 $c = curl_init();
 curl_setopt($c, CURLOPT_HEADER, 0);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
@@ -18,13 +97,38 @@ curl_setopt($c, CURLOPT_URL, 'http://newcar.xcar.com.cn/63/review/0.htm');
 $newcar = curl_exec($c);
 curl_close($c);
 
-// 抓取过来的页面转换为 utf-8 中文编码
 $newcar_u = iconv("GBK","UTF-8//IGNORE", $newcar);
 
-// 全部点评开始
 $newcar_1 = explode('全部点评</a></div>', $newcar_u);
-// 全部点评结束
 $newcar_2 = explode('<div class="pagers">', $newcar_1[1]);
+$newcar_3s = explode('<dl><dt><em>点评角度：', $newcar_2[0]);
 
-// 测试解析后的内容
-echo $newcar_2[0];
+array_shift($newcar_3s);
+
+foreach ($newcar_3s as $newcar_3sk) {
+    $newcar_4s = explode('</a></em>', $newcar_3sk);
+    $na_1s = substr($newcar_4s[0], -6);
+    echo '<hr>'.$na_1s;
+    $newcar_5s = explode('】<a href="http://www.xcar.com.cn/', $newcar_4s[1]);
+    $na_2s = substr($newcar_5s[0], 3);
+    echo '&nbsp;'.$na_2s;
+    // echo '<hr>'.$newcar_5s[1];
+    $update_time = date('Y-m-d H:i:s', time());
+    $sql = "insert into xcar_newcar (
+            na_1s,
+            na_2s,
+            update_time
+        )
+        values (
+            '{$na_1s}',
+            '{$na_2s}',
+            '{$update_time}'
+        )";
+    if ($sql_write_p == 'on') {
+        $ret = mysqli_query($con, $sql);
+        if(!$ret) {
+            echo 'mysqli_query error';
+            exit(0);
+        }
+    }
+}
